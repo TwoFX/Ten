88d33527173acc8efd4c3221c238d50193bc1d71
Markus Himmel <markus@himmel-villmar.de>
2016-07-12 21:09:05 +0200

    Add a not very good bot

---
+++ Ten/Ten/FirstBot.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ten
+{
+	class FirstBot : IMoveProvider
+	{
+		private int scoreMove(IReadOnlyGameState state, Move m)
+		{
+			List<Tuple<int, int>> occ = Enumerable.Range(0, Tile.TILE_BOUNDS)
+				.SelectMany(i => Enumerable.Range(0, Tile.TILE_BOUNDS).Select(j => new { i, j }))
+				.Where(p => state.NextMoves[m.Choice].Contention[p.i, p.j])
+				.Select(x => Tuple.Create(m.X + x.i, m.Y + x.j)).ToList();
+
+			int score = 0;
+			for (int i = 0; i < state.FieldSizeX; i++)
+			{
+				if (Enumerable.Range(0, state.FieldSizeY).All(j => state.Field[i, j] != null || occ.Contains(Tuple.Create(i, j))))
+					score += 10;
+
+				if (Enumerable.Range(0, state.FieldSizeY).All(j => state.Field[i, j] == null) && Enumerable.Range(0, state.FieldSizeY).Any(j => occ.Contains(Tuple.Create(i, j))))
+					score += -2;
+			}
+
+			for (int j = 0; j < state.FieldSizeY; j++)
+			{
+				if (Enumerable.Range(0, state.FieldSizeX).All(i => state.Field[i, j] != null || occ.Contains(Tuple.Create(i, j))))
+					score += 10;
+
+				if (Enumerable.Range(0, state.FieldSizeX).All(i => state.Field[i, j] == null) && Enumerable.Range(0, state.FieldSizeX).Any(i => occ.Contains(Tuple.Create(i, j))))
+					score += -5;
+			}
+
+			foreach (Tile cand in Tile.All)
+			{
+				for (int i = 0; i < state.FieldSizeX; i++)
+				{
+					for (int j = 0; j < state.FieldSizeY; j++)
+					{
+						if (Enumerable.Range(0, Tile.TILE_BOUNDS).SelectMany(x => Enumerable.Range(0, Tile.TILE_BOUNDS).Select(y => new { x, y }))
+							.Where(p => cand.Contention[p.x, p.y]).All(p => i + p.x < state.FieldSizeX && j + p.y < state.FieldSizeY && state.Field[i + p.x, j + p.y] == null && !occ.Contains(Tuple.Create(i + p.x, j + p.y))))
+							goto found;
+					}
+				}
+				score -= 4;
+				found:;
+			}
+
+			return score;
+		}
+
+		public Move GetNextMove(IReadOnlyGameState state)
+		{
+			List<Move> candidates = Enumerable.Range(0, state.NextMoves.Count)
+				.SelectMany(c => Enumerable.Range(0, state.FieldSizeX)
+				.SelectMany(x => Enumerable.Range(0, state.FieldSizeY)
+				.Select(y => new Move(c, x, y))))
+				.Where(m => state.IsValidMove(m)).ToList();
+
+			return candidates.OrderByDescending(x => scoreMove(state, x)).ThenBy(x => x.X).First();
+		}
+	}
+}
--- Ten/Ten/Program.cs
+++ Ten/Ten/Program.cs
@@ -10,12 +10,15 @@ namespace Ten
 {
 	class Program
 	{
+		static int sizeX = 10;
+		static int sizeY = 10;
+		static int moves = 3;
 
 		[STAThread]
 		static void Main(string[] args)
 		{
 			var app = new Application();
-			var win = new GameWindow(10, 10);
+			var win = new GameWindow(sizeX, sizeY);
 			Thread gameThread = new Thread(RunGame);
 			gameThread.Start(win);
 			app.Run(win);
@@ -25,12 +28,12 @@ namespace Ten
 		{
 			GameWindow win = gameWindow as GameWindow;
 			var ci = new ConsoleInteraction();
-			var bot = new RandomBot();
-			var game = new Game(10, 10, 3, bot);
+			var bot = new FirstBot();
+			var game = new Game(sizeX, sizeY, moves, bot);
 			if (win != null)
 				game.AddObserver(win);
 			game.AddObserver(ci);
-			game.Run(500);
+			game.Run(100);
 		}
 	}
 }
